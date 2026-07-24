df7ef9f90b7fff36c12fff7363bdf0f5d2dc04ff
Agustin Mendez <agustin_mendez99@outlook.com>
2026-07-24 21:06:25 +0000

    Create GET endpoints

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planet, Favorite
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,14 +28,19 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
@@ -44,6 +50,72 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+# Obtener todos los personajes
+@app.route("/people", methods=["GET"])
+def get_people():
+
+    people = People.query.all()
+
+    return jsonify(
+        [person.serialize() for person in people]
+    ), 200
+
+
+# Obtener un personaje por su id
+@app.route("/people/<int:people_id>", methods=["GET"])
+def get_one_person(people_id):
+
+    person = People.query.get(people_id)
+
+    if person is None:
+        return jsonify({
+            "msg": "Character not found"
+        }), 404
+
+    return jsonify(
+        person.serialize()
+    ), 200
+
+
+# Obtener todos los planetas
+@app.route("/planets", methods=["GET"])
+def get_planets():
+
+    planets = Planet.query.all()
+
+    return jsonify(
+        [planet.serialize() for planet in planets]
+    ), 200
+
+
+# Obtener un planeta por su id
+@app.route("/planets/<int:planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+
+    planet = Planet.query.get(planet_id)
+
+    if planet is None:
+        return jsonify({
+            "msg": "Planet not found"
+        }), 404
+
+    return jsonify(
+        planet.serialize()
+    ), 200
+
+
+# Obtener todos los usuarios
+@app.route("/users", methods=["GET"])
+def get_users():
+
+    users = User.query.all()
+
+    return jsonify(
+        [user.serialize() for user in users]
+    ), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
